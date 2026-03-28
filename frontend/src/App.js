93207f736b04cc2ae2bfd4967538bc5430165f77
kavin2006-spec <kavinsagar2006@gmail.com>
2026-03-28 13:19:16 +0100

    render deployement

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API = "https://nominatively-beechen-dewey.ngrok-free.dev";
+const API = "http://localhost:8000/";
 
 const TEAM_COLORS = {
   "Mercedes":          "#00D2BE",
